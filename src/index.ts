2654f0a69225cb8cf3a7e6c7980436108101fb46
Gal Schlezinger <gal@spitfire.co.il>
2024-09-01 12:40:35 +0300

    expose grammyjs types (#11)
    * expose grammyjs types
    
    * add changeset

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@ export type * from "./client";
 export type * from "./types";
 export * from "./build-message";
 export type * from "./build-message";
+
+import type * as Telegram from "@grammyjs/types";
+
+export type {
+  /**
+   * Telegram Types as exposed from grammyjs directly
+   */
+  Telegram,
+};
